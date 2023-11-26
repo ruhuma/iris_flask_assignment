3d78dbc82d36b8bcb802e096faa49eef9697e570
ruhuma <141302425+ruhuma@users.noreply.github.com>
2023-11-26 12:18:29 +0000

    final for deploym

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import pickle
+import numpy as np
 
 app = Flask(__name__)
 
@@ -10,14 +12,21 @@ def hello():
 def predict():
     if request.method == 'POST':
         sep_len = request.form['sl']
+        print(sep_len)
         sep_width = request.form['sw']
         petal_len = request.form['pl']
+        print(petal_len)
         petal_width = request.form['pw']
         model = pickle.load(open('iris.pkl','rb'))
         input=np.array([[sep_len,sep_width,petal_len,petal_width]]).astype(np.float64)
         pred=model.predict(input)
-        print(pred)
-        return render_template('prediction.html', output=sep_width)
+        if pred == 0:
+            classification = 'Setosa'
+        elif pred== 1:
+            classification = 'Vercicolor'
+        else :
+            classification = 'Virginica'
+        return render_template('prediction.html', output=classification)
     else:
         return render_template('prediction.html')
     
